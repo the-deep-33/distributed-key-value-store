a36c0d16fb5a42a493d96e9e42705b1c491a28f2
the-deep-33 <petnik033@gmail.com>
2026-04-11 23:28:41 +0200

    Set up clusters of the server and made the outline of the main part of the architecture (node.py)

---
+++ config.py
@@ -0,0 +1,7 @@
+"""Cluster of nodes which represent the servers in the store"""
+
+CLUSTER = {
+    1: ("127.0.0.1", 5001),
+    2: ("127.0.0.1", 5002),
+    3: ("127.0.0.1", 5003),
+}
---
+++ node.py
@@ -0,0 +1,43 @@
+from dataclasses import dataclass, field
+from config import CLUSTER
+from models import LogEntry, RequestVote, VoteResponse, AppendEntries, AppendEntriesResponse
+import argparse
+import socket
+from threading import Thread
+
+@dataclass
+class Node:
+    """All the elements that a node needs, regardless if it is a leader or a follower"""
+    id: int
+    leader_id: int = None
+    current_term: int = 0
+    state: str = "follower"
+    commit_index: int = 0
+    last_applied: int = 0
+    log: list = field(default_factory = list)
+    store: dict = field(default_factory = dict)
+    next_index: dict = field(default_factory = dict)
+    voted_for: int = None
+
+    def start(self):
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        ADRESA = CLUSTER[self.id]
+        self.server.bind(ADRESA)
+        self.server.listen()
+        
+        while True:
+            conn, addr = self.server.accept()
+            t = Thread(target = self.handle_client, args = (conn, addr))
+            t.start()
+
+    def handle_connection(self, conn, addr):
+        pass
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--id", type = int, help = "ID of the node you want to create")
+    args = parser.parse_args()
+    node = Node(id=args.id)
+    print(f"Kreiran node sa id: {node.id}, state = {node.state}, term = {node.current_term}")
+    node.start()
